5e316906c1c14959d795d79a8757dcb31d72ce2b
Swagat Konchada <swagatata@gmail.com>
2022-11-26 06:57:07 +0530

    trying getrandom

--- c/main.c
+++ c/main.c
@@ -1,7 +1,24 @@
 #include "stdlib.h"
 #include "stdio.h"
+#include "sys/random.h"
+
+void try_random_library() {
+  unsigned char buf[16];
+  int rc = getrandom(buf, sizeof(buf), 0);
+  if (rc < 0) {
+    perror("getrandom");
+    exit(1);
+  }
+  for (int i = 0; i < sizeof(buf); i++) {
+    printf("%02x", buf[i]);
+  }
+  printf("\n");
+}
 
 int main() {
+  try_random_library();
+  return 0;
+
   for (int i = 0; i < 10; i++) {
     printf("Random number: %ld\n", random());
   }
