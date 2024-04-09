f92266d6c8954a71a185c6f289e6c86efc5ba417
Swagat Konchada <skonchada@bloomberg.net>
2024-04-09 22:42:41 +0530

    working cpp directory with cmake and google test

---
+++ cpp/distributed_systems/include/distributed_system.h
@@ -0,0 +1,9 @@
+#ifndef DISTRIBUTED_SYSTEM_H
+#define DISTRIBUTED_SYSTEM_H
+
+class DistributedSystem {
+public:
+    // Add your class member functions and variables here
+};
+
+#endif // DISTRIBUTED_SYSTEM_H
