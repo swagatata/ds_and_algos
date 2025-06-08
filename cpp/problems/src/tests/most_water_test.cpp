3fff8622eb05f28339fe6c599b10a255dee96320
Swagat <swagatata@gmail.com>
2025-06-08 23:49:50 +0100

    fix algorithm

--- cpp/problems/src/tests/most_water_test.cpp
+++ cpp/problems/src/tests/most_water_test.cpp
@@ -13,35 +13,34 @@ public:
 
   unsigned long long solution() {
     DEBUG_LOG("Solving problem for") PV(d_towers)
-    vector<unsigned int> leftMax(d_towers.size(), 0);
-    vector<unsigned int> rightMax(d_towers.size(), 0);
-
-    leftMax[0] = 0;
-    for (int i = 1; i < d_towers.size(); ++i) {
-      leftMax[i] = (d_towers[i] > d_towers[leftMax[i - 1]] ? i : leftMax[i - 1]);
-    }
-    DEBUG_LOG("Printing leftMax")
-    PV(leftMax)
-
-    // rightMax[d_towers.size() - 1] = d_towers.back();
-    // for (int i = d_towers.size() - 2; i >= 0; --i) {
-    //   rightMax[i] = (d_towers[i] > rightMax[i + 1] ? d_towers[i] : rightMax[i + 1]);
-    // }
+    
+    if (d_towers.size() < 2) return 0;
+    
     unsigned long long maxArea = 0;
-    for (int i = 1; i < d_towers.size(); ++i) {
-      unsigned long long area = (i - leftMax[i - 1]) * min(d_towers[i], d_towers[leftMax[i - 1]]); 
-      DEBUG_LOG("Area when i is " << i << " is " << area) 
-      if (area > maxArea)
-        maxArea = area;
+    int left = 0;
+    int right = d_towers.size() - 1;
+    
+    while (left < right) {
+      // Calculate current area
+      unsigned long long currentArea = (right - left) * min(d_towers[left], d_towers[right]);
+      DEBUG_LOG("Current area between " << left << " and " << right << " is " << currentArea)
+      
+      // Update max area if current area is larger
+      maxArea = max(maxArea, currentArea);
+      
+      // Move the pointer pointing to the smaller height
+      if (d_towers[left] < d_towers[right]) {
+        left++;
+      } else {
+        right--;
+      }
     }
-
+    
     return maxArea;
   }
 };
 
-
 class MostWaterTest : public BaseTest, public ::testing::WithParamInterface<tuple<vector<unsigned int>, unsigned long long>> {
-
 };
 
 TEST_P(MostWaterTest, EdgeCases) {
@@ -54,10 +53,14 @@ INSTANTIATE_TEST_SUITE_P(
   MostWaterTest,
   MostWaterTest,
   ::testing::Values(
-    make_tuple(vector<unsigned int>{1, 1}, 1)
-    ,make_tuple(vector<unsigned int>{1, 0}, 0)
-    ,make_tuple(vector<unsigned int>{1, 2}, 1)
-    ,make_tuple(vector<unsigned int>{2, 2}, 2)
-    ,make_tuple(vector<unsigned int>{2, 1, 2}, 4)
+    make_tuple(vector<unsigned int>{1, 1}, 1),
+    make_tuple(vector<unsigned int>{1, 0}, 0),
+    make_tuple(vector<unsigned int>{1, 2}, 1),
+    make_tuple(vector<unsigned int>{2, 2}, 2),
+    make_tuple(vector<unsigned int>{2, 1, 2}, 4),
+    make_tuple(vector<unsigned int>{1, 1, 2, 1, 1}, 4),
+    make_tuple(vector<unsigned int>{1, 8, 6, 2, 5, 4, 8, 3, 7}, 49),
+    make_tuple(vector<unsigned int>{4, 3, 2, 1, 4}, 16),
+    make_tuple(vector<unsigned int>{1, 2, 1}, 2)
   ) 
 );
